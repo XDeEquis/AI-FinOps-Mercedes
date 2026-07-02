dad53a1a184f1b7781dc5652b745e6b4b06a3dba
Alewuito <alejg411@uma.es>
2026-07-02 10:29:43 +0200

    Fallito

--- backend/app.js
+++ backend/app.js
@@ -36,9 +36,4 @@ app.use(function (err, req, res, next) {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor proxy FinOps corriendo en http://localhost:${PORT}`);
-});
-
 module.exports = app;
